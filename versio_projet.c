b93ea8ea84edf9854f479fcd3093ab7213ca5c52
fantabae08-ctrl <fantabae08@gmail.com>
2026-02-17 22:39:07 +0000

    deuxieme commit

--- versio_projet.c
+++ versio_projet.c
@@ -12,6 +12,15 @@ int main(){
             puts("veuillez saisir un entier negatif:");
         scanf("%d",&x);
         }while(x>0);
+        printf("le nombre que tu as saisi est %d",x);
+        scanf("%d",&x);
+        }while(x>0);
+        if(x%2==0){
+            somme==somme%x;
+            cpt++;
+        }
+
+
     }
 
 return 0 ;
